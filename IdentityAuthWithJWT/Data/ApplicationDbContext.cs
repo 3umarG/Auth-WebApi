7815ce90bba4344f4305c723f92c53e7cab20d3b
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-07-11 00:31:50 +0200

    Add Initial DB Setup for Identity

---
+++ IdentityAuthWithJWT/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityAuthWithJWT.Data
+{
+	public class ApplicationDbContext :IdentityDbContext<ApiUser>
+	{
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+
+			builder.Entity<ApiUser>().ToTable("Users");
+			builder.Entity<IdentityRole>().ToTable("Roles");
+			builder.Entity<IdentityUserRole<string>>().ToTable("UserRoles");
+			builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins");
+			builder.Entity<IdentityUserToken<string>>().ToTable("UserTokens");
+			builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims");
+			builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaims");
+		}
+	}
+}
---
+++ IdentityAuthWithJWT/Extensions/ServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using IdentityAuthWithJWT.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IdentityAuthWithJWT.Extensions
+{
+	public static class ServiceExtensions
+	{
+		public static void ConfigureIdentity(this IServiceCollection services)
+		{
+			var builder = services.AddIdentityCore<ApiUser>();
+
+			builder = new IdentityBuilder(builder.UserType, typeof(IdentityRole) , services);
+
+			builder.AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+		} 
+	}
+}
