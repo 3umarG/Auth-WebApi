8740fb414657b587d3969da2acf7fb25853e2731
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-07-11 00:46:44 +0200

    Add Auth Controller & inject UserManager , SignInManager

---
+++ IdentityAuthWithJWT/Controllers/AuthController.cs
@@ -0,0 +1,21 @@
+﻿using IdentityAuthWithJWT.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityAuthWithJWT.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AuthController : ControllerBase
+	{
+		private readonly UserManager<ApiUser> _userManager;
+		private readonly SignInManager<ApiUser> _signInManager;
+
+		public AuthController(SignInManager<ApiUser> signInManager, UserManager<ApiUser> userManager)
+		{
+			_userManager = userManager;
+			_signInManager = signInManager;
+		}
+	}
+}
