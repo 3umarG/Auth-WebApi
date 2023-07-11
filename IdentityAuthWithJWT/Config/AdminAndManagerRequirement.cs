809cf74d1560197c6828e4b352d746fb52d3f8fc
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-07-12 00:38:09 +0200

    Add Custom Handler for creating AdminAndManager Policy

---
+++ IdentityAuthWithJWT/Config/AdminAndManagerRequirement.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace IdentityAuthWithJWT.Config
+{
+	public class AdminAndManagerRequirement : IAuthorizationRequirement
+	{
+	}
+}
---
+++ IdentityAuthWithJWT/Config/AdminAndManagerRequirementHandler.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace IdentityAuthWithJWT.Config
+{
+	public class AdminAndManagerRequirementHandler : AuthorizationHandler<AdminAndManagerRequirement>
+	{
+		protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, AdminAndManagerRequirement requirement)
+		{
+			var user = context.User;
+			if (user.IsInRole("Admin") && user.IsInRole("Manager"))
+			{
+				context.Succeed(requirement);
+			}
+
+			return Task.CompletedTask;
+		}
+	}
+
+	// Register the custom requirement handler to configure it on the Program.cs 
+	public static class AuthorizationServiceExtensions
+	{
+		public static void AddAdminAndManagerRequirement(this IServiceCollection services)
+		{
+			services.AddSingleton<IAuthorizationHandler, AdminAndManagerRequirementHandler>();
+		}
+	}
+
+}
