6f51380f2ead90d49543f88cb614729445a32cec
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-07-13 14:03:24 +0200

    Configure Swagger and Authorization Headers

--- IdentityAuthWithJWT/Program.cs
+++ IdentityAuthWithJWT/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Net;
 using System.Text;
 
@@ -18,7 +19,62 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Configure Swagger 
+builder.Services.AddSwaggerGen(options =>
+{
+	// Change the main schema for the Swagger
+	options.SwaggerDoc("v1", new OpenApiInfo
+	{
+		Version = "v1",
+		Title = "MoviesApi",
+		Description = "My first api",
+		TermsOfService = new Uri("https://www.google.com"),
+		Contact = new OpenApiContact
+		{
+			Name = "DevCreed",
+			Email = "test@domain.com",
+			Url = new Uri("https://www.google.com")
+		},
+		License = new OpenApiLicense
+		{
+			Name = "My license",
+			Url = new Uri("https://www.google.com")
+		}
+	});
+
+	// Add Global Authorization for all controllers with their end point
+	options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+	{
+		Name = "Authorization",
+		Type = SecuritySchemeType.ApiKey,
+		Scheme = "Bearer",
+		BearerFormat = "JWT",
+		In = ParameterLocation.Header,
+		Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\""
+	});
+
+	// Add Specific Authorization option for every end point
+	options.AddSecurityRequirement(new OpenApiSecurityRequirement
+	{
+		{
+			new OpenApiSecurityScheme
+			{
+				Reference = new OpenApiReference
+				{
+					Type = ReferenceType.SecurityScheme,
+					Id = "Bearer"
+				},
+				Name = "Bearer",
+				In = ParameterLocation.Header
+			},
+			new List<string>()
+		}
+	});
+});
+
+
+
 // add AutoMapper Service
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
