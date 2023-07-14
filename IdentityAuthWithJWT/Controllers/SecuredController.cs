0e77699bc0d6387f20709a41141546d3921bc9e7
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-07-14 03:28:40 +0200

    Make Custom Controller for Authorized End points

---
+++ IdentityAuthWithJWT/Controllers/SecuredController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityAuthWithJWT.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	[Authorize]
+	public class SecuredController : ControllerBase
+	{
+
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return Ok("Get Successfully");
+		}
+	}
+}
