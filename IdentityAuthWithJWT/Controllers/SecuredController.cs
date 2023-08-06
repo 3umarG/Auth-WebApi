69775ccd6d8e41f8ad29fac23af74be9d66c4ca9
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-08-06 13:54:50 +0200

    Add Decode JWT information method .

--- IdentityAuthWithJWT/Controllers/SecuredController.cs
+++ IdentityAuthWithJWT/Controllers/SecuredController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace IdentityAuthWithJWT.Controllers
 {
@@ -13,7 +14,18 @@ namespace IdentityAuthWithJWT.Controllers
 		[HttpGet]
 		public IActionResult Get()
 		{
-			return Ok("Get Successfully");
+			string token = Request.Headers.Authorization.First().Split(" ")[1];
+			string email = extractUserInfoFromJwtToken(token , "uid");
+			return Ok(email);
+		}
+
+		private static string extractUserInfoFromJwtToken(string token , string info)
+		{
+			var handler = new JwtSecurityTokenHandler();
+			var jwtSecurityToken = handler.ReadJwtToken(token);
+
+			var email = jwtSecurityToken.Claims.First(claim => claim.Type == info).Value;
+			return email;
 		}
 	}
 }
